335b2ce46c6b617f8bf418bc9a9c1e7d44fe72bc
mohammadibrah <mohammad.si.tt@gmail.com>
2021-11-02 20:24:41 +0300

    2 files modified, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CourcesComponent } from './cources.component';
-import { TestComponent } from './test/test.component';
 import { courcesServis } from './cources.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CourcesComponent, TestComponent],
+  declarations: [ AppComponent, HelloComponent, CourcesComponent],
   providers: [
     courcesServis
   ],
